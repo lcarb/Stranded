1d47eb14d47ebf6a1de12a627e27d21b16307e4f
lcarb <52737500+lcarb@users.noreply.github.com>
2019-09-01 17:38:17 +0800

    Update main.lua
    simple player movement test

--- main.lua
+++ main.lua
@@ -1,3 +1,21 @@
+function love.load()
+
+  player = {}
+  player.x = 400;
+  player.y = 300;
+
+  love.keyboard.setKeyRepeat(true)
+
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("d") then
+    player.x = player.x+1
+  elseif love.keyboard.isDown("a") then
+    player.x = player.x-1
+  end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.circle("fill", player.x, player.y, 5)
 end
